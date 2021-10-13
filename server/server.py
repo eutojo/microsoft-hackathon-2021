217420d913a849dd554abc097bd83518842bb002
Tomofred <you@example.com>
2021-10-13 15:43:39 +1100

    backend

--- server/server.py
+++ server/server.py
@@ -1,19 +1,24 @@
-from flask import Flask
-from DataService import DataService
+from flask import Flask, jsonify
+import DataService
 import time
 
 app = Flask(__name__)
-data_service = DataService()
-data_service.load_data()
 
 @app.route("/")
 def index():
     return "Hello World!"
 
-@app.route("/floor/<int:id>")
-def get_floor(id):
-    return data_service.get_floor(id)
+@app.route("/buildings/get")
+def get_buildings():
+    data = DataService.get_buildings()
+    return jsonify(data)
 
-@app.route("/building/<int:id>")
-def get_building(id):
-    return data_service.get_building(id)
+@app.route("/buildings/get/<building_id>")
+def get_building_by_id(building_id):
+    data = DataService.get_building_info_by_id(building_id)
+    return jsonify(data)
+
+@app.route("/buildings/get/<building_id>/<floor_id>")
+def get_floor_info_by_id(building_id, floor_id):
+    data = DataService.get_floor_info_by_id(building_id, floor_id)
+    return jsonify(data)
