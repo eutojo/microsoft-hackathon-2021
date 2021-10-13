cb89e07866037a1bd2a31d2eaddbd5d24f35ddae
Eunike <eunikeutojo98@gmail.com>
2021-10-13 11:36:21 +1100

    Extracted script

--- server/DataService.py
+++ server/DataService.py
@@ -2,45 +2,12 @@ import json
 from PIL import Image
 import numpy as np
 from flask import jsonify
-from bs4 import BeautifulSoup
 
 sample_floor_fn = "./rawMap/sample_floor_metadata.json"
 sample_ground_fn = "./rawMap/sample_floor_metadata.json"
 sample_foundry = "./rawMap/building_data/foundry_data/FDY-L1.json"
-
-IMAGE_PATH = "./rawImages/foundry_data/"
-SVG = ".svg"
-
-def scrapeSVG(image):
-    with open(image) as fp:
-        soup = BeautifulSoup(fp, 'html.parser')
-
-    svg = soup.find_all('svg')
-    for item in svg:
-        viewbox = item['viewbox']
-
-    rect = soup.find_all('rect')
-    floors = []
-
-    for item in rect:
-        if (item["fill"] == "#ffffff"):
-            className = "blank"
-        elif (item["fill"] == "#f5f5f5"):
-            className = "booked"
-        else:
-            className = "free"
-
-        floors.append({
-            "type":"rect",
-            "x": item["x"],
-            "y": item["y"],
-            "width": item["width"],
-            "height": item["height"],
-            "stroke": item["stroke"],
-            "className": className
-        })
-    
-    return {"floors": floors, "viewbox": viewbox}
+BUILDING_DATA_PATH = "./rawMap/building_data/foundry_data/FDY-L"
+JSON = ".json"
 
 class DataService:
     floors = None
@@ -59,14 +26,10 @@ class DataService:
         return True
 
     def get_floor(self, floor_id):
-        floors = scrapeSVG(IMAGE_PATH + str(floor_id) + SVG)
-
-        with open(sample_foundry, 'r') as file:
+        with open(BUILDING_DATA_PATH + str(floor_id) + JSON, 'r') as file:
             data = json.load(file)
-
-        data.update(floors)
         
-        return jsonify(data)     
+        return data 
         
     def get_building(self, building_id):
         return self.levels
