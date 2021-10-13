4590f26c4f023a440995f47649e42a29589d40a4
Eunike <eunikeutojo98@gmail.com>
2021-10-13 19:48:25 +1100

    New view setup

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,11 +1,30 @@
 import React from "react";
 import NewMeeting from './pages/NewMeeting'
+import Calendar from './pages/Calendar'
+
+import { BsToggleOff, BsToggleOn } from "react-icons/bs"
 
 export default class Home extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            toggle: true
+        }
+        
+        this.toggle = this.toggle.bind(this)
+    }
+
+    toggle(){
+        this.setState({
+            toggle: !this.state.toggle
+        })
+    }
+    
     render(){
         return(
             <div className="container">
-                <NewMeeting />
+                {this.state.toggle ? <NewMeeting /> : <Calendar />}
+                <h1 className="toggle">{this.state.toggle ? <BsToggleOn onClick={() => this.toggle()} /> : <BsToggleOff onClick={() => this.toggle()}/>}</h1>
             </div>
         );
     }
---
+++ client/src/pages/Calendar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { RiBearSmileLine } from "react-icons/ri"
+
+export default class Calendar extends React.Component {
+    render(){
+        return(
+            <div className="window-container">
+                Hi Freddy Bear <RiBearSmileLine />
+            </div>
+        );
+    }
+}
