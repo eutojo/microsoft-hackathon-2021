52d74c73b3290200e6e3d4504961c5ca42fa724a
Tomofred <you@example.com>
2021-10-13 22:51:41 +1100

    mobile view
    Co-authored-by: Eunike Utojo <eunikeutojo98@gmail.com>

---
+++ client/src/assets/components/calender-view/MapView.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+export default class MapDetails extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            rooms: [],
+            viewbox: ''
+        }
+
+        this.getRooms = this.getRooms.bind(this)
+    }
+
+    componentDidMount(){
+        this.getRooms()
+    }
+
+    async getRooms(){
+        fetch("/buildings/get/FDY/FDY-L1")
+        .then(res => res.json())
+        .then(res => this.setState({
+            rooms: res['floor_data']['room'],
+            viewbox: res['floor_data']['viewbox']
+        }))
+    }
+
+    render(){
+        return(
+            <div className="mobile-map">
+                <svg
+                        style={{'width': '100%', 'border': '1px solid white'}}
+                        xmlns="http://www.w3.org/2000/svg"
+                        id="Layer_1"
+                        data-name="Layer 1"
+                        viewBox={this.state.viewbox != '' ? this.state.viewbox : ''}
+                    >
+                {this.state.rooms.length > 0 && this.state.rooms.map((entry) => 
+                            entry['type'] == 'rect' ?
+                            <rect 
+                                x={entry["x"]}
+                                y={entry["y"]}
+                                width={entry["width"]}
+                                height={entry["height"]}
+                                fill={entry["fill"]}
+                                stroke={entry["stroke"]}
+                                className={entry["className"]}
+                                id={entry["room_id"]}
+                                /> :
+                            <div></div>
+                        )}
+                    </svg>
+            </div>    
+        )
+    }
+}
--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -1,11 +1,72 @@
-import React from "react";
-import { RiBearSmileLine } from "react-icons/ri"
-
+import React from "react"
+import {IoIosArrowBack} from 'react-icons/io'
+import {ImBin} from 'react-icons/im'
+import { FaCircle } from 'react-icons/fa';
+import {AiFillCheckCircle} from 'react-icons/ai'
+import { IoLocationOutline} from 'react-icons/io5';
+import MapDetails from "../assets/components/calender-view/MapView";
 export default class Calendar extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            toggle: false
+        }
+        
+        this.toggle = this.toggle.bind(this)
+    }
+
+    toggle(){
+        this.setState({
+            toggle: !this.state.toggle
+        })
+    }
+
     render(){
         return(
-            <div className="window-container">
-                Hi Freddy Bear <RiBearSmileLine />
+            <div className="window-container mobile-container">
+                
+                {/* map here */}
+                <div className="mobile">
+                    <div className="tab">
+                        <h3><IoIosArrowBack /></h3>
+                        <h3>Calender</h3>
+                        <h3><ImBin /></h3>
+                    </div>
+                    <div className="info-container">
+                        <div className="info-section">
+                            <h2><FaCircle style={{'fill': '#3787e9'}}/></h2>
+                            <div className="data">
+                                <h2>General Monthly Catcbup</h2>
+                                <h3>Thursday, 29 February 2022, 10:30am</h3>
+                            </div>
+                        </div>
+                        <div className="info-section">
+                            <h2><AiFillCheckCircle style={{'fill': 'green'}} /></h2>
+                            <div className="data">
+                                <h2>Accepted</h2>
+                            </div>
+                        </div>
+                        <div className="info-section">
+                            <div className="data last">
+                                <h3>Hello Mate, looking good there for a catchup hahahahhahaahhahahahah</h3>
+                            </div>
+                        </div>
+                    </div>    
+                    <div className="info-container">
+                        <div className="info-section">
+                            <h2><IoLocationOutline /></h2>
+                            <div className="data">
+                                <h2>Foundry Level 1 Room 08</h2>
+                            </div>
+                            <div className="data-button" onClick={() => this.toggle()}>
+                                <h2>Navigate</h2>
+                            </div>
+                        </div>
+                        <div className="info-section map">
+                            {this.state.toggle && <MapDetails />}
+                        </div>    
+                    </div>    
+                </div>
             </div>
         );
     }
